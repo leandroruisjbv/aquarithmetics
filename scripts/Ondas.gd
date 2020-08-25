97101f63f01763d9744b7a10ad811e89f47b134a
leandroruisjbv <40581238+leandroruisjbv@users.noreply.github.com>
2020-08-25 17:07:33 -0300

    Teleporte
    A lógica está toda no script, porém invertida e meio bagunçada. Vamos lá:
    Com o passar do tempo, a velocidade de subida da água aumenta, pois multiplica-se a gravidade por delta a cada novo delta. Errar faz com que esse efeito seja ainda mais drástico (não completamente implementado, como avisei, mas a lógica está lá)
    O outro sinal de controle, o de acerto, deve "teleportar" a água, alguns pixels para baixo, mantendo o jogador "no jogo" por mais tempo. Todos os valores serão ajustados, incluindo a posição inicial, velocidade, taxa de aumento e afins.

--- scripts/Ondas.gd
+++ scripts/Ondas.gd
@@ -1,32 +1,37 @@
 extends KinematicBody2D
 
-export (int) var speed = 120
-export (int) var jump_speed = -180
-export (int) var gravity = -400
+export (int) var speed = 200
+#export (int) var speed = 120
+#export (int) var jump_speed = -180
+export (int) var gravity = -2
 
-var velocity = Vector2.ZERO
+var velocity = Vector2()
+
+#var velocity = Vector2.ZERO
 var not_stoped = true
 
 func _physics_process(delta):
+
+	velocity = move_and_slide(velocity)
 	velocity = move_and_slide(velocity, Vector2.UP)
-	if not_stoped : velocity.y += gravity * delta
+	if not_stoped :
+		velocity.y += gravity * delta
+		print(velocity.y)
 
 func _on_Topo_body_entered(body):
 	print("entrou")
 	not_stoped = false
 	velocity.y = 0
-	pass # Replace with function body.
 	
 func processa_sinais(sinal: String):
 	if sinal == ("acertou"):
-		#velocity.x += speed
+		velocity.y += 1
 		print("agua recebeu acerto")
 
 	if sinal == ("errou"):
-		#velocity.x -= speed
+		position.y = position.y + 200
+		velocity.y -= 1
 		print("agua recebeu erro")
-	
 
-	
-	
+
 	
